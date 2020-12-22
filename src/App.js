3168b990e5dec017ccbeade8d98581415d01463a
Ajay Sharma <63399965+ajay-aditatechnologies@users.noreply.github.com>
2020-12-22 18:36:45 +0530

    Calling a folder

--- src/App.js
+++ src/App.js
@@ -4,8 +4,17 @@ function App() {
   return  (<div>
     <h3>hello</h3>
     <h2>'hello how are you'</h2>
+    <Folder />
+    <Folder />
+    <Folder />
   </div>);
 
 }
+
+const Folder = () => {
+  return <h4> Folder</h4>
+
+}
+
 
 export default App;
