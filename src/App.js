0a27b9c32cdebb243f50dca2912a4bd98bbac235
Ajay Sharma <63399965+ajay-aditatechnologies@users.noreply.github.com>
2020-12-23 12:06:32 +0530

    Props.name with Div

--- src/App.js
+++ src/App.js
@@ -1,18 +1,21 @@
 import React from 'react';
 
 function App() {
-  return  (<div>
+  return (<div>
     <h3>hello</h3>
     <h2>'hello how are you'</h2>
-    <Folder food="cake" drink="coffee" />
+    <Folder name="cake" name1="hui" />
+    <Folder name="cake2" />
   </div>);
 
 }
 
 const Folder = (props) => {
-  console.log(props)
-  const name = 'my_desktop'
-  return <h4> {name} </h4>
+  return (<div>
+    <h4> {props.name} </h4>
+    <h4> {props.name1} </h4>
+  </div>
+  )
 
 }
 
